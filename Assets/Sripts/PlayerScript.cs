ae39b98a385f7148181910c8d92427122c72bb47
Oliver Hakansson <theoliverhakansson@gmail.com>
2026-04-18 10:52:27 -0700

    adding the deck indicator and blood

---
+++ Assets/Sripts/PlayerScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{   
+    [SerializeField] public int currentDeck = 1;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
