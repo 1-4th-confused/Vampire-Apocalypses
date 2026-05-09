2296c2b57a006571a0e978c2e2e37748a58a0cd8
Rui Rebich Hespanha <Rui.hespanha@gmail.com>
2026-05-08 18:19:13 -0700

    added json things

--- Assets/Sripts/CardManager.cs
+++ Assets/Sripts/CardManager.cs
@@ -1,59 +1,34 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+
 
 [System.Serializable]
 public class CardData
 {
     public double damage;
     public double defense;
-    public string imagePath;
+    public string imageName;
 }
 
 public class CardManager : MonoBehaviour 
 {
-    [SerializeField] private string cardName;
-    
-    private CardData currentCard;
-    private Sprite cardSprite;
+    public ArrayList cardTypes = new ArrayList();
+    public ArrayList cardImages = new ArrayList();
 
     void Start()
     {
-        LoadCardData(cardName);
-    }
+        string[] cardJsons = Directory.GetFiles(@"Assets\Resources", "*.json");
 
-    void LoadCardData(string name)
-    {
-        // 1. Define the path to the JSON file
-        // Using Resources.Load is easiest for cross-platform builds
-        string jsonFilePath = "Cards/JSON/" + name; 
-        TextAsset targetFile = Resources.Load<TextAsset>(jsonFilePath);
-
-        if (targetFile != null)
+        foreach(string file in cardJsons)
         {
-            // 2. Deserialize the JSON
-            currentCard = JsonUtility.FromJson<CardData>(targetFile.text);
-            Debug.Log($"Loaded {name}: Damage {currentCard.damage}, Defense {currentCard.defense}");
-
-            // 3. Load the Image based on the path stored inside the JSON
-            // Note: imagePath in JSON should not include the extension (.png)
-            string spritePath = "Cards/Images/" + currentCard.imagePath;
-            cardSprite = Resources.Load<Sprite>(spritePath);
-
-            if (cardSprite != null)
-            {
-                Debug.Log("Card Sprite successfully loaded!");
-                // If you have a SpriteRenderer or Image component, assign it here:
-                // GetComponent<SpriteRenderer>().sprite = cardSprite;
-            }
-            else
-            {
-                Debug.LogError($"Sprite not found at: {spritePath}");
-            }
-        }
-        else
-        {
-            Debug.LogError($"JSON file not found for: {name} at {jsonFilePath}");
+            string name = file;
+            name = file.Substring(file.IndexOf(@"\")+1);
+            name = name.Substring(name.IndexOf(@"\")+1);
+            name = name.Substring(0,name.IndexOf(@"."));
+            cardTypes.Add(JsonUtility.FromJson<CardData>(Resources.Load<TextAsset>(name).ToString()));
+            cardImages.Add(Resources.Load<Sprite>(name+"Image").ToString());
         }
     }
 }
